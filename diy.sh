20c26f41467d4b0c93be8da2f9096958321cdf99
NS0296 <110940186+NS0296@users.noreply.github.com>
2024-12-14 21:38:53 -0300

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@ sudo chmod -R 777 /config
 sudo chmod -R 777 /defaults
 
 # 添加火狐浏览器桌面图标以abc用户运行，因为root用户不允许运行火狐浏览器
-cat <<EOL > ~/Desktop/firefox-abc.desktop
+cat <<EOL > /config/Desktop/firefox-abc.desktop
 [Desktop Entry]
 Version=1.0
 Name=Firefox (Run as abc)
@@ -19,9 +19,9 @@ Categories=Network;WebBrowser;
 EOL
 
 # 设置快捷方式文件的权限
-chmod +x ~/Desktop/firefox-abc.desktop
+chmod +x /config/Desktop/firefox-abc.desktop
 
-cat <<EOL > ~/Desktop/vlc-abc.desktop
+cat <<EOL > /config/Desktop/vlc-abc.desktop
 [Desktop Entry]
 Version=1.0
 Name=vlc (Run as abc)
@@ -34,5 +34,5 @@ Categories=Multimedia;Player;Video;
 EOL
 
 # 设置快捷方式文件的权限
-chmod +x ~/Desktop/vlc-abc.desktop
+chmod +x /config/Desktop/vlc-abc.desktop
 
